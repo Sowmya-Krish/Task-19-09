41697a514ff3e0cb91be5501c98542d311f41a3a
Sowmya-Krish <113003820+Sowmya-Krish@users.noreply.github.com>
2022-09-20 09:41:07 +0530

    Add files via upload

---
+++ Task 19-09.js
@@ -0,0 +1,10 @@
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12]
+let odds = arr.filter(n => n%2)
+console.log (odds)
+
+
+var array = [1, 2, 3];
+for (var i = 0, sum = 0; i < array.length; sum += array[i++]);
+console.log(sum);
+
+
